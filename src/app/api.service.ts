f2285a94bc4822c83eb1e7a25947180a3f80a2f8
Yusuf <adebakinyusuf03@gmail.com>
2023-08-20 01:36:13 +0100

    Creating an api service

---
+++ src/app/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'https://random-data-api.com/api/v2/users?size=10&is_json=true'
+
+  constructor(private http: HttpClient) { }
+
+  getData(page: number, pageSize: number): Observable<any>{
+    const url = `${this.apiUrl}?page=${page}&pageSize=${pageSize}`;
+    return this.http.get(url);
+  }
+}

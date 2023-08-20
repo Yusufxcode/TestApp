4996757e53820137d7c81cb738b5f4249ec17bfd
Yusuf <adebakinyusuf03@gmail.com>
2023-08-20 01:56:02 +0100

    Implement the pagination logic

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.css']
 })
-export class DataTableComponent {
+export class DataTableComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'email', 'Phone_number'];
+  dataSource: any[] = [];
+  pageSize = 10;
+  currentPage = 1;
+  totalItems = 0;
 
+  constructor(private apiservice: ApiService) {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(): void {
+    this.apiservice.getData(this.currentPage, this.pageSize).subscribe((data: any) => {
+      this.dataSource = data.totalItems;
+      this.totalItems = data.totalItems;
+    });
+  }
+
+  onPageChange(event: any): void {
+    this.currentPage = event.pageIndex + 1;
+    this.pageSize = event.pageSize;
+    this.loadData();
+  }
+  
 }
